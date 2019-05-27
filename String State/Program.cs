f9abcba2d1917c5751bf2d5bfa373d2b0ff0a61f
Otávio FQA Almeida <otavioalmeida650@gmail.com>
2019-05-27 19:19:20 -0300

    Char Array instead of String Builder

--- String State/Program.cs
+++ String State/Program.cs
@@ -5,36 +5,37 @@ namespace String_State
 {
     class Program
     {
-    const char CharLayout = '_';
+        const char CharLayout = '_';
         static void Main(string[] args)
         {
             string FinalState = "PALAVRA".ToUpper(); // Palavra Alvo || Estado Final
-            StringBuilder ActualState = new StringBuilder(FinalState.Length); // Palavra Atual || Estado Atual
-            ActualState.Append(CharLayout, FinalState.Length); // Preenche o estado com CharLayout
+            // Dá pra dar um outro jeito para criar esse array e popular ele com o charLayout, mas assim fica criativo :)
+            char[] ActualState = new StringBuilder(FinalState.Length).Append(CharLayout, FinalState.Length).ToString().ToCharArray(); // Palavra Atual || Estado Atual, já Preenche o estado com CharLayout e converte para charArray
             char input;
 
-            while (true){ // Repete até o Estado Final e o Atual serem iguais
+            while (true)
+            { // Repete até o Estado Final e o Atual serem iguais
                 input = Console.ReadLine().ToCharArray()[0]; // Lê o primeiro character do console, deve ter um jeito melhor de fazer isso :(
                 if (FinalState.Contains(input)) // Verifica se o Estado Final contém o char que o usuário digitou
                 {
-                    for(int aux = FinalState.IndexOf(input); aux < FinalState.Length; aux++) // Se contém, acha os indexes desse character, começando no primeiro
+                    for (int aux = FinalState.IndexOf(input); aux < FinalState.Length; aux++) // Se contém, acha os indexes desse character, começando no primeiro
                     {
-                        if(FinalState[aux] == input) // Para cada vez que achar
+                        if (FinalState[aux] == input) // Para cada vez que achar
                         {
-                            if(ActualState[aux] == CharLayout) // Checa se essa posição está vazia no Estado Atual
+                            if (ActualState[aux] == CharLayout) // Checa se essa posição está vazia no Estado Atual
                             {
-                                ActualState.Replace(CharLayout, input, aux, 1); // Se estiver, troca a posição pelo caracter desejado
+                                ActualState[aux] = input; // Se estiver, troca a posição pelo caracter desejado
                                 break; // Break para não trocar em todas as ocorrências
                             }
                         }
                     }
                 }
-                if(FinalState == ActualState.ToString()) // Final :)
+                if (FinalState == new String(ActualState)) // Final :)
                 {
                     Console.WriteLine("Congrats");
                     Environment.Exit(0);
                 }
-                Console.WriteLine(String.Format("Final: {0}\nAtual: {1}", FinalState, ActualState)); // Mostra o estado atual do sistema
+                Console.WriteLine(String.Format("Final: {0}\nAtual: {1}", FinalState, new String(ActualState))); // Mostra o estado atual do sistema
             }
         }
     }
